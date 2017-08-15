f71da4f3df6c295611e04d5d6aa1a2bb1af66ccd
Parallel.Gh <parallelgh@outlook.com>
2017-08-15 17:28:32 +0800

    Front_end
    The first stage of h5.

--- README.txt
+++
@@ -1,3 +0,0 @@
-Hello, GuoZhen!
-My name is GaoHui,nice to meet you!
-Let's go to H5 class.
--- h5.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("Welcome to H5 class!\n");
-    return 0;
-}
-
